32f8eb383923c15dc9eac9da386f776910786317
Mateus Romano <mateusyasd@hotmail.com>
2020-08-16 15:37:39 -0300

    Updates

---
+++ precache-manifest.8b288c9d7aebcef34f0eeeadeea34aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab7757ed959882cf339f4723150d8fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ecb924e4b21575f514c",
+    "url": "/static/css/main.5771e938.chunk.css"
+  },
+  {
+    "revision": "7969e83f3b178eb6143e",
+    "url": "/static/js/2.b06abf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b06abf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ecb924e4b21575f514c",
+    "url": "/static/js/main.8965a5c2.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
